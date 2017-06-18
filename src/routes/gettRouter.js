2aa22817a9dbf99cf92adc8bd328fce5a4482afe
AfikNof <afiknof9@gmail.com>
2017-06-18 21:48:32 +0300

    Code refactoring

--- src/routes/gettRouter.js
+++ src/routes/gettRouter.js
@@ -15,20 +15,6 @@ router.get('/driver/:id', function (req, res, next) {
     });
 });
 
-router.post('/import/local', function (req, res, next) { //imports local json file
-    jsonReader().then(function (data) {
-        DriversDAL.insertDrivers(data).then(function () {
-            res.send({status: 200, message: 'Import ended successfully!'});
-        })
-            .catch(function (error) {
-                res.send(error);
-            });
-    }).catch(function (error) {
-        res.send(error);
-    });
-
-
-});
 
 router.post('/import', function (req, res, next) { //imports file from form-data, if undefined imports the default local json Gett.json
     var form = new formidable.IncomingForm();
@@ -53,3 +39,21 @@ router.post('/import', function (req, res, next) { //imports file from form-data
 });
 
 module.exports = router;
+
+
+/*
+ router.post('/import/local', function (req, res, next) { //imports local json file
+ jsonReader().then(function (data) {
+ DriversDAL.insertDrivers(data).then(function () {
+ res.send({status: 200, message: 'Import ended successfully!'});
+ })
+ .catch(function (error) {
+ res.send(error);
+ });
+ }).catch(function (error) {
+ res.send(error);
+ });
+
+
+ });
+ */
